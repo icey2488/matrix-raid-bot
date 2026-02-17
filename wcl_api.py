87f49c94e32d40e38fff85968151b383917be230
Icehunter <git.cheer770@passinbox.com>
2026-02-16 19:18:06 -0800

    chore: remove legacy monolith files replaced by modular architecture

--- wcl_api.py
+++
@@ -1,9 +0,0 @@
-import requests
-import os
-
-def get_wcl_token():
-    auth = (os.getenv("WCL_CLIENT_ID"), os.getenv("WCL_CLIENT_SECRET"))
-    resp = requests.post("https://www.warcraftlogs.com/oauth/token", data={"grant_type": "client_credentials"}, auth=auth)
-    return resp.json().get("access_token")
-
-# Logic to verify code on profile would go here
